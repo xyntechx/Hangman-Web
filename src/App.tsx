04385142a7eea21eb2dc4fcdc251bffd09bb28d1
xyntechx <xyntechx@gmail.com>
2023-09-29 08:10:09 -0700

    ✨ Process word and configure blanks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+    const WORD = import.meta.env.VITE_WORD; // load word from .env.local
+    const [hiddenWord, setHiddenWord] = useState<string[]>([]);
+    // const [livesLeft, setLivesLeft] = useState(7); // standard hangman rules (if there even is any :p)
+    // const [isGameOver, setIsGameOver] = useState(false); // true when livesLeft === 0
+
+    useEffect(() => {
+        const blanks: string[] = [];
+
+        for (let i = 0; i < WORD.length; i++) {
+            const char = WORD[i];
+            if (char === " ") blanks.push(" ");
+            else blanks.push("__");
+        }
+
+        setHiddenWord(blanks);
+    }, []);
+
     return (
         <main className="main">
             <h1>Hangman</h1>
+            {/* <p>Lives left: {livesLeft}</p> */}
+            <p>
+                {hiddenWord.map((char) => (
+                    <span className="blanks">{char}</span>
+                ))}
+            </p>
         </main>
     );
 }
